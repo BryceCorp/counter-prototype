ee2d0916f4f51ebcd4f54ea78e783960b0fa99f6
BryceCorp <deltafx25@ymail.com>
2020-08-28 15:33:32 +0100

    edit of git-ignore

---
+++ build/precache-manifest.86eefe1f9672d55a6d27b802cda45a31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b85947e1e308f91a589ffa1ad93125c",
+    "url": "/counter-prototype/index.html"
+  },
+  {
+    "revision": "13cb4da978c15aabf6dd",
+    "url": "/counter-prototype/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "c75af6e995fdf33dd8a8",
+    "url": "/counter-prototype/static/css/main.a15c7f7c.chunk.css"
+  },
+  {
+    "revision": "13cb4da978c15aabf6dd",
+    "url": "/counter-prototype/static/js/2.989a5cbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-prototype/static/js/2.989a5cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75af6e995fdf33dd8a8",
+    "url": "/counter-prototype/static/js/main.d50ec105.chunk.js"
+  },
+  {
+    "revision": "1edff50084df1d117a53",
+    "url": "/counter-prototype/static/js/runtime-main.5f5915c0.js"
+  }
+]);
